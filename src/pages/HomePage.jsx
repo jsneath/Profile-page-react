8ee71dd23eb642056015cad66995be7c0d81b7b6
jsneath <jamessneath1@gmail.com>
2024-01-12 19:50:36 +0000

    Added route, navbar, multiple page set up

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/header'; // Adjust the path according to your project structure
+// import './HomePage.css'; // If you have a specific CSS file for the HomePage
+
+function HomePage() {
+    return (
+        <div className="homePage">
+            <NavBar /> 
+
+        </div>
+    );
+}
+
+export default HomePage;
