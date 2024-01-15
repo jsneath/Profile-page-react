8b1d3e88fdf097ad97eaa2bb40cc3837d02a51b6
jsneath <jamessneath1@gmail.com>
2024-01-15 20:34:16 +0000

    changed carousel to cards and added remaining projects

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import NavBar from '../components/header'; // Adjust the path according to your project structure
-import { Carousel } from 'react-bootstrap';
 import projectsData from '../data/projects.json';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function ProjectsPage() {
     return (
         <div className="projectsPage">
             <NavBar /> 
-            <Carousel>
             {projectsData.map((project) => (
-                    <Carousel.Item key={project.id}>
-                        <img src={project.image} alt={project.title} className="d-block w-100" />
-                        <Carousel.Caption>
-                            <h3>{project.title}</h3>
-                            <p>{project.description}</p>
-                            <a href={project.link} target="_blank" rel="noopener noreferrer">View Project</a><br/>
-                            <a href={project.github} target="_blank" rel="noopener noreferrer">GitHub</a>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                ))}
-            </Carousel>
+                <Card style={{ width: '18rem' }} key={project.id}>
+                <Card.Img variant="top" src={project.image} alt={project.title} className="d-block" />
+                <Card.Body>
+                    <Card.Title>{project.title}</Card.Title>
+                    <Card.Text>
+                    {project.description}
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item><Card.Link href={project.link}>Website link</Card.Link></ListGroup.Item>
+                    <ListGroup.Item><Card.Link href={project.github}>Github link</Card.Link></ListGroup.Item>
+                </ListGroup>
+                </Card>
+            ))}
         </div>
     );
 }
 
 export default ProjectsPage;
+
