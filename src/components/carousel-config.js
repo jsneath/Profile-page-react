bdd1793aa62ed89ffa5225a79d16af135feeb444
jsneath <jamessneath1@gmail.com>
2024-09-30 20:33:19 +0100

    added carousel

---
+++ src/components/carousel-config.js
@@ -0,0 +1,50 @@
+import { faHourglass1 } from "@fortawesome/free-solid-svg-icons";
+
+export const cardDetails = {
+  0: {
+    imgUrl: "./src/assets/html.svg",
+    title: "Text 1",
+  },
+
+  1: {
+    imgUrl: "./src/assets/css.svg",
+    title: "Text 2",
+  },
+
+  2: {
+    imgUrl: "./src/assets/js.svg",
+    title: "Text 3",
+  },
+
+  3: {
+    imgUrl: "./src/assets/react.svg",
+    title: "Text 4",
+  },
+
+  4: {
+    imgUrl: "./src/assets/ruby.svg",
+    title: "Text 5",
+  },
+
+  5: {
+    imgUrl: "./src/assets/next.svg",
+    title: "Text 6",
+  },
+
+  6: {
+    imgUrl: "./src/assets/node.svg",
+    title: "Text 7",
+  },
+  7: {
+    imgUrl: "./src/assets/tailwind.svg",
+    title: "Text 8",
+  },
+  8: {
+    imgUrl: "./src/assets/github.svg",
+    title: "Text 9",
+  },
+  9: {
+    imgUrl: "./src/assets/figma.svg",
+    title: "Text 10",
+  },
+};
