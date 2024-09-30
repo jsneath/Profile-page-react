3fdfed3776673c1f0568ca3d93297054afeb082c
jsneath <jamessneath1@gmail.com>
2024-09-30 20:52:05 +0100

    moved images to public folder for netlify

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,82 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
-import myImage from '../assets/-1tgddd.jpg';
-import '../App.css'
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+import myImage from "/assets/-1tgddd.jpg";
+import "../App.css";
 
 function NavBar() {
-    return (
-<nav className="navbar bg-body-tertiary fixed-top">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">My Profile</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  return (
+    <nav className="navbar bg-body-tertiary fixed-top">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          My Profile
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="offcanvas offcanvas-end"
+          tabIndex="-1"
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+        >
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+              Menu
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/projects" className="nav-link">
+                  My Projects
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link">
+                  Contact me
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="offcanvas-body">
-        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/projects" className="nav-link">My Projects</Link>
-          </li>
-          <li className="nav-item">
-          <Link to="/contact" className="nav-link">Contact me</Link>
-          </li>
-
-        </ul>
-      </div>
-    </div>
-  </div>
-</nav>
-
-    );
+    </nav>
+  );
 }
-
 
 function Jumbotron() {
   return (
     <div className="jumbotron jumbotron-fluid my-jumbotron-bg">
-    <div className="container-fluid text-on-image">
-    <h1 className="display-4">James Sneath</h1>
-    <p className="lead">Welcome, to my personal portfolio website where i show off my latest projects</p>
-    <img src={myImage} alt="A picture of me" className="profile-image" />
-  </div>
-</div>
-  )
+      <div className="container-fluid text-on-image">
+        <h1 className="display-4">James Sneath</h1>
+        <p className="lead">
+          Welcome, to my personal portfolio website where i show off my latest
+          projects
+        </p>
+        <img src={myImage} alt="A picture of me" className="profile-image" />
+      </div>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
 export { Jumbotron };
--- src/components/carousel-config.js
+++ src/components/carousel-config.js
@@ -2,49 +2,49 @@ import { faHourglass1 } from "@fortawesome/free-solid-svg-icons";
 
 export const cardDetails = {
   0: {
-    imgUrl: "./src/assets/html.svg",
+    imgUrl: "/assets/html.svg",
     title: "Text 1",
   },
 
   1: {
-    imgUrl: "./src/assets/css.svg",
+    imgUrl: "./assets/css.svg",
     title: "Text 2",
   },
 
   2: {
-    imgUrl: "./src/assets/js.svg",
+    imgUrl: "/assets/js.svg",
     title: "Text 3",
   },
 
   3: {
-    imgUrl: "./src/assets/react.svg",
+    imgUrl: "/assets/react.svg",
     title: "Text 4",
   },
 
   4: {
-    imgUrl: "./src/assets/ruby.svg",
+    imgUrl: "/assets/ruby.svg",
     title: "Text 5",
   },
 
   5: {
-    imgUrl: "./src/assets/next.svg",
+    imgUrl: "/assets/next.svg",
     title: "Text 6",
   },
 
   6: {
-    imgUrl: "./src/assets/node.svg",
+    imgUrl: "/assets/node.svg",
     title: "Text 7",
   },
   7: {
-    imgUrl: "./src/assets/tailwind.svg",
+    imgUrl: "/assets/tailwind.svg",
     title: "Text 8",
   },
   8: {
-    imgUrl: "./src/assets/github.svg",
+    imgUrl: "/assets/github.svg",
     title: "Text 9",
   },
   9: {
-    imgUrl: "./src/assets/figma.svg",
+    imgUrl: "/assets/figma.svg",
     title: "Text 10",
   },
 };
