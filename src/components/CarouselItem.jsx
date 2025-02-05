76d11919ec570b0ba59f5e71d6dda827a8546848
jsneath <jamessneath1@gmail.com>
2025-02-05 21:20:30 +0000

    changed colors of icons

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function CarouselItem({ imgUrl, imgTitle }) {
+export default function CarouselItem({ imgUrl, title }) {
   return (
     <div className="carousel-card">
-      <img src={imgUrl} alt={imgTitle}></img>
+      <img src={imgUrl} alt={title}></img>
     </div>
   );
 }
